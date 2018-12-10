6b1604e33bc3f7c7fa003c664bf4ece773418b1e
maoyuchaxue <wangjt15@mails.tsinghua.edu.cn>
2018-12-11 00:00:50 +0800

    init git for baidu sdk

---
+++ README.md
@@ -0,0 +1,8 @@
+# Baidu Java ITMA SDK Demo, modified for personal use
+
+All external binaries files/jars will not be contained, please refer to [original source](http://ai.baidu.com/sdk#itma).
+
+## running guide from original SDK
+
+使用 sh gradlew run  --no-daemon 运行
+也可以使用 sh run-test.sh 运行
---
+++ src/main/java/com/baidu/aip/demotest/AudioCapturer.java
@@ -0,0 +1,43 @@
+package com.baidu.aip.demotest;
+
+import javax.sound.sampled.*;
+
+public class AudioCapturer {
+
+    final public static int packageDurationInMs = 40;
+    final public static int sampleRate = 8096;
+    final public static int sampleSizeInBits = 16;
+
+    private TargetDataLine line;
+
+    public AudioCapturer() {
+        AudioFormat format = new AudioFormat(sampleRate, sampleSizeInBits, 1, true, false);
+        DataLine.Info info = new DataLine.Info(TargetDataLine.class, 
+            format);
+        if (!AudioSystem.isLineSupported(info)) {
+            System.out.println("Error: no valid DataLine found");
+        }
+
+        try {
+            line = (TargetDataLine) AudioSystem.getLine(info);
+            line.open(format);
+        } catch (LineUnavailableException ex) {
+            System.out.println("Error: cannot open capture device");
+        }
+    }
+
+    public void start() {
+        line.start();
+    }
+
+    public byte[] read() {
+        int len = packageDurationInMs * sampleSizeInBits;
+        byte[] bytes = new byte[len];
+        line.read(bytes, 0, len);
+        return bytes;
+    }
+
+    public void stop() {
+        line.stop();
+    }
+}
