e6fe995811bae6b714425a61eb35587ee9dd9686
maoyuchaxue <wangjt15@mails.tsinghua.edu.cn>
2019-03-08 10:08:14 +0800

    added button device controller

--- server/devices/button-device.js
+++ server/devices/button-device.js
@@ -3,20 +3,13 @@ const process = require('process');
 
 const host = "localhost";
 const idev_port = 9000;
-const odev_port = 9001;
 
 var idev_socket = net.createConnection(idev_port, host);
 idev_socket.write("screen");
 
-var odev_socket = net.createConnection(odev_port, host);
-odev_socket.write("screen");
-
-odev_socket.on('data', function (data) {
-    console.log(data);
-})
-
 process.stdin.resume();
+process.stdin.setRawMode(true);
 
-process.stdin.on('data', function () {
+process.stdin.on('data', function (k) {
     idev_socket.write("1");
 });
